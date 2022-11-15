bcbb3aa594001b55d80f12eaafdd9018a3270b4a
Urfan <7nquqmr@code.edu.az>
2022-11-15 22:14:23 +0300

    task c#

---
+++ csharpbackend/Program.cs
@@ -0,0 +1,26 @@
+﻿class Task
+{
+    private static void Main(){
+
+        Console.WriteLine("Enter numbers");
+        int number= Convert.ToInt32(Console.ReadLine());
+        int [] array=new int[number];
+        int sum=0;
+
+        for(int i=0;i<number;i++){
+            Console.WriteLine("element["+(i+1)+"]:");
+            array[i]=int.Parse(Console.ReadLine());
+        if(array[i]%2==0){
+
+                sum+=array[i];
+            }
+
+        }
+       
+
+        Console.WriteLine("even numbers sum="+sum);
+
+        
+
+    }
+}
