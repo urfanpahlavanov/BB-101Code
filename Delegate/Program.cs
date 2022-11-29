a7ff006627b07af19ea4e2648220523c0fb090b4
Urfan <7nquqmr@code.edu.az>
2022-11-30 00:22:42 +0300

    Delegate,Generic

---
+++ Delegate/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Cryptography.X509Certificates;
+
+Console.WriteLine("*******Delegate Generics*******");
+int[] arr = { 1, 2, -1, 3, 4, 5 };
+Check check = new Check(Return);
+foreach (var item in FindAll(arr, check))
+{
+    Console.WriteLine(item);
+}
+
+
+ int[] FindAll(int[] arr,Check func)
+{
+    int[] arr1 = { };
+    foreach(int item in arr)   
+    {
+        if (func(item))
+        {
+            arr1.Append(item);  
+        }
+    }
+    return arr1;
+    
+}
+
+bool Return(int item) => item > 2;
+
+
+delegate bool Check(int item);
