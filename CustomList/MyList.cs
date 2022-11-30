d1d9f6d897dfb0ea1ed0ec3b04b3e25a92d102af
Urfan <7nquqmr@code.edu.az>
2022-11-30 23:39:31 +0300

    Custom List

---
+++ CustomList/MyList.cs
@@ -0,0 +1,59 @@
+﻿
+using System.Collections;
+
+namespace CustomList
+{
+    public class MyList<T>
+    {
+        public int Capacity { get; set; }
+        public T[] list;
+        static int Count;
+
+        public MyList(int capacity)
+        {
+            Capacity = capacity;
+            list = new T[capacity];
+        }
+
+        public void Add(T item)
+        {
+            if (Capacity == 0)
+            {
+                Capacity = 2;
+                Array.Resize<T>(ref list, Capacity);
+                list[0] = item;
+                Count++;
+                return;
+
+            }
+            if (Capacity == Count)
+            {
+                Capacity *= 2;
+                Array.Resize<T>(ref list, Capacity);
+                list[Capacity / 2] = item;
+                Count++;
+                return;
+            }
+            list[Count ] = item;
+            Count++;
+
+        }
+        public bool Exsist(Predicate<T> fumc)
+        {
+            for (int i = 0; i < list.Length; i++)
+            {
+                if (fumc(list[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+      public T this[int index]
+        {
+            get { return list[index]; }
+            set { list[index] = value; }    
+        }
+    }
+}
---
+++ CustomList/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CustomList;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        MyList<string> myList = new MyList<string>(5);
+
+        myList.Add("urfan");
+        myList.Add("resul");
+        myList.Add("nasir");
+        myList.Add("elcin");  
+        myList.Add("sona");
+        
+        foreach (var item in myList.list)
+        {
+            Console.WriteLine(item);
+           
+        }
+       
+    }
+
+      
+}
