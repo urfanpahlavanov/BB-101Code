d3aa6efd1cfeea1eda9fd7665739692c4c2c3c2a
Urfan <7nquqmr@code.edu.az>
2022-12-07 22:23:00 +0300

    Directoryc#

---
+++ ProectDirectory/Program.cs
@@ -0,0 +1,30 @@
+﻿
+string path = @"C:\Users\CP\source\repos\ProectDirectory\Folder\";
+
+Folders(path);
+void  Folders(string path,string cavity=" ")
+{
+    var return1=Directory.GetDirectories(path);
+    var return2=Directory.GetFiles(path);
+
+    foreach(var item in return1)
+    {
+        Console.WriteLine(cavity+item);
+        if (Directory.CreateDirectory(item).Exists)
+        {
+            cavity=cavity+item; 
+            Folders((string)item,cavity);   
+        }
+        else
+        {
+            cavity = String.Empty;
+            Folders(path,cavity);
+        }
+    }
+    foreach(var item in return2)
+    {
+        Console.WriteLine(cavity+item);
+    }
+
+}
+
